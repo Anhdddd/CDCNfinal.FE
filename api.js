ece57cd866ff7315c5b46b57d36716281f517011
Anhdddd <tranngoctin77@gmail.com>
2022-11-23 23:49:52 +0700

    Feature show Product

---
+++ api.js
@@ -0,0 +1,32 @@
+const endpoint = 'http://duongtruongvu21.ddns.net:8888/api/Product';
+const ul = document.getElementById('containerProduct')
+
+let bankHolidays;
+let england;
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => handleDates(data));
+
+function handleDates(data) {
+    bankHolidays = data;
+    const html = bankHolidays.map((items) => {
+        return `
+						<div class="col-2th">
+							<div class="card">
+								<div class="image">
+									<img src="uploads/12333.png">
+								</div>
+								<div class="name">
+									<p>${items.productName}</p>
+								</div>
+								<div class="price">
+									<p>${items.price} VND</p>
+								</div>
+							</div>
+						</div>
+                
+						`;
+    }).join('');
+    ul.innerHTML = html;
+}
